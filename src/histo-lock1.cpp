054711625ce35b39ecf4a1dc4b0b9e25eefe45bc
lxyWS <348677673@qq.com>
2025-12-19 15:08:03 +0800

    Add files via upload

---
+++ src/histo-lock1.cpp
@@ -0,0 +1,224 @@
+// 本文件实现的是基于Test-and-Set (TAS)的自旋锁
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <cassert>
+#include <pthread.h>
+#include "Timer.h"
+
+extern "C"
+{
+#include "ppmb_io.h"
+}
+
+struct img
+{
+    int xsize;
+    int ysize;
+    int maxrgb;
+    unsigned char *r;
+    unsigned char *g;
+    unsigned char *b;
+};
+
+int g_thread_num;
+
+void print_histogram(FILE *f, int *hist, int N)
+{
+    fprintf(f, "%d\n", N + 1);
+    for (int i = 0; i <= N; i++)
+    {
+        fprintf(f, "%d %d\n", i, hist[i]);
+    }
+}
+
+// TAS锁
+struct Tas_lock
+{
+    volatile int is_locked; // 0表示锁是自由的，1表示锁被占用
+};
+
+// 初始化锁，一开始未被占用
+void init_lock(Tas_lock *lock)
+{
+    lock->is_locked = 0;
+}
+
+// 获取锁
+void acquire_lock(Tas_lock *lock)
+{
+    while (__sync_lock_test_and_set(&lock->is_locked, 1))
+    {
+    }
+}
+
+// 释放锁
+void release_lock(Tas_lock *lock)
+{
+    __sync_lock_release(&lock->is_locked);
+}
+
+struct ThreadArg
+{
+    img *input;
+    int *hist_r;
+    int *hist_g;
+    int *hist_b;
+    int start, end;
+    Tas_lock *locks_r;
+    Tas_lock *locks_g;
+    Tas_lock *locks_b;
+};
+
+void *thread_histo(void *arg)
+{
+    ThreadArg *t = (ThreadArg *)arg;
+    for (int i = t->start; i < t->end; ++i)
+    {
+        // 获取该位置的rgb值
+        unsigned char r = t->input->r[i];
+        unsigned char g = t->input->g[i];
+        unsigned char b = t->input->b[i];
+
+        acquire_lock(&t->locks_r[r]);
+        ++t->hist_r[r];
+        release_lock(&t->locks_r[r]);
+
+        acquire_lock(&t->locks_g[g]);
+        ++t->hist_g[g];
+        release_lock(&t->locks_g[g]);
+
+        acquire_lock(&t->locks_b[b]);
+        ++t->hist_b[b];
+        release_lock(&t->locks_b[b]);
+    }
+
+    return nullptr;
+}
+
+void histogram(struct img *input, int *hist_r, int *hist_g, int *hist_b)
+{
+    // we assume hist_r, hist_g, hist_b are zeroed on entry.
+    int length = input->xsize * input->ysize;
+    int each_len = length / g_thread_num;
+    int maxrgb = input->maxrgb;
+
+    if (g_thread_num == 1)
+    {
+        for (int pix = 0; pix < length; pix++)
+        {
+            hist_r[input->r[pix]] += 1;
+            hist_g[input->g[pix]] += 1;
+            hist_b[input->b[pix]] += 1;
+        }
+
+        return;
+    }
+
+    // 为rgb的每个取值分别设置自己的锁
+    Tas_lock *locks_r = (Tas_lock *)malloc((maxrgb + 1) * sizeof(Tas_lock));
+    Tas_lock *locks_g = (Tas_lock *)malloc((maxrgb + 1) * sizeof(Tas_lock));
+    Tas_lock *locks_b = (Tas_lock *)malloc((maxrgb + 1) * sizeof(Tas_lock));
+
+    // 为所有锁初始化
+    for (int i = 0; i <= maxrgb; ++i)
+    {
+        init_lock(&locks_r[i]);
+        init_lock(&locks_g[i]);
+        init_lock(&locks_b[i]);
+    }
+
+    pthread_t *threads = new pthread_t[g_thread_num];
+    ThreadArg *args = new ThreadArg[g_thread_num];
+
+    for (int i = 0; i < g_thread_num; ++i)
+    {
+        args[i].input = input;
+        args[i].hist_r = hist_r;
+        args[i].hist_g = hist_g;
+        args[i].hist_b = hist_b;
+        args[i].start = i * each_len;
+        args[i].end = (i == g_thread_num - 1) ? length : (i + 1) * each_len;
+        args[i].locks_r = locks_r;
+        args[i].locks_g = locks_g;
+        args[i].locks_b = locks_b;
+    }
+
+    for (int i = 0; i < g_thread_num; ++i)
+    {
+        pthread_create(&threads[i], nullptr, thread_histo, &args[i]);
+    }
+
+    for (int i = 0; i < g_thread_num; ++i)
+    {
+        pthread_join(threads[i], nullptr);
+    }
+
+    free(locks_r);
+    free(locks_g);
+    free(locks_b);
+
+    delete[] threads;
+    delete[] args;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        printf("Usage: %s input-file output-file threads\n", argv[0]);
+        printf("       For single-threaded runs, pass threads = 1\n");
+        exit(1);
+    }
+
+    char *output_file = argv[2];
+    char *input_file = argv[1];
+    int threads = atoi(argv[3]);
+
+    g_thread_num = threads;
+
+    /* remove this in multithreaded version */
+    // if(threads != 1) {
+    //   printf("ERROR: Only supports single-threaded execution\n");
+    //   exit(1);
+    // }
+
+    struct img input;
+
+    if (!ppmb_read(input_file, &input.xsize, &input.ysize, &input.maxrgb,
+                   &input.r, &input.g, &input.b))
+    {
+        if (input.maxrgb > 255)
+        {
+            printf("Maxrgb %d not supported\n", input.maxrgb);
+            exit(1);
+        }
+
+        int *hist_r, *hist_g, *hist_b;
+
+        hist_r = (int *)calloc(input.maxrgb + 1, sizeof(int));
+        hist_g = (int *)calloc(input.maxrgb + 1, sizeof(int));
+        hist_b = (int *)calloc(input.maxrgb + 1, sizeof(int));
+
+        ggc::Timer t("histogram");
+
+        t.start();
+        histogram(&input, hist_r, hist_g, hist_b);
+        t.stop();
+
+        FILE *out = fopen(output_file, "w");
+        if (out)
+        {
+            print_histogram(out, hist_r, input.maxrgb);
+            print_histogram(out, hist_g, input.maxrgb);
+            print_histogram(out, hist_b, input.maxrgb);
+            fclose(out);
+        }
+        else
+        {
+            fprintf(stderr, "Unable to output!\n");
+        }
+        printf("Time: %llu ns\n", t.duration());
+    }
+}
---
+++ src/histo-lockfree.cpp
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <cassert>
+#include <atomic>
+#include <pthread.h>
+#include "Timer.h"
+
+extern "C"
+{
+#include "ppmb_io.h"
+}
+
+int g_thread_num;
+
+struct img
+{
+    int xsize;
+    int ysize;
+    int maxrgb;
+    unsigned char *r;
+    unsigned char *g;
+    unsigned char *b;
+};
+
+void print_histogram(FILE *f, int *hist, int N)
+{
+    fprintf(f, "%d\n", N + 1);
+    for (int i = 0; i <= N; i++)
+    {
+        fprintf(f, "%d %d\n", i, hist[i]);
+    }
+}
+
+// thread_histo 函数的参数
+struct ThreadArg
+{
+    img *input;
+    std::atomic<int> *atomic_r; // 需要原子加法，保证不会出现错误
+    std::atomic<int> *atomic_g;
+    std::atomic<int> *atomic_b;
+    int start, end;
+};
+
+void *thread_histo(void *arg)
+{
+    ThreadArg *t = (ThreadArg *)arg;
+    for (int i = t->start; i < t->end; ++i)
+    {
+        ++t->atomic_r[t->input->r[i]];
+        ++t->atomic_g[t->input->g[i]];
+        ++t->atomic_b[t->input->b[i]];
+    }
+
+    return nullptr;
+}
+
+void histogram(struct img *input, int *hist_r, int *hist_g, int *hist_b)
+{
+    // we assume hist_r, hist_g, hist_b are zeroed on entry.
+    int length = input->xsize * input->ysize;
+    int each_len = length / g_thread_num;
+    int maxrgb = input->maxrgb;
+
+    std::atomic<int> *atomic_r = reinterpret_cast<std::atomic<int> *>(hist_r);
+    std::atomic<int> *atomic_g = reinterpret_cast<std::atomic<int> *>(hist_g);
+    std::atomic<int> *atomic_b = reinterpret_cast<std::atomic<int> *>(hist_b);
+
+    if (g_thread_num == 1)
+    {
+        for (int pix = 0; pix < length; pix++)
+        {
+            atomic_r[input->r[pix]] += 1;
+            atomic_g[input->g[pix]] += 1;
+            atomic_b[input->b[pix]] += 1;
+        }
+
+        return;
+    }
+
+    pthread_t *threads = new pthread_t[g_thread_num];
+    ThreadArg *args = new ThreadArg[g_thread_num];
+
+    // 设置参数
+    for (int i = 0; i < g_thread_num; ++i)
+    {
+        args[i].input = input;
+        args[i].start = i * each_len;
+        args[i].end = (i == g_thread_num - 1) ? length : (i + 1) * each_len;
+        args[i].atomic_r = atomic_r; // 原子加法时，无需分配私有空间，直接在公共空间上做修改
+        args[i].atomic_g = atomic_g;
+        args[i].atomic_b = atomic_b;
+    }
+
+    for (int i = 0; i < g_thread_num; ++i)
+    {
+        pthread_create(&threads[i], nullptr, thread_histo, &args[i]);
+    }
+
+    // 直接回收，无需合并
+    for (int i = 0; i < g_thread_num; ++i)
+    {
+        pthread_join(threads[i], nullptr);
+    }
+
+    delete[] threads;
+    delete[] args;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        printf("Usage: %s input-file output-file threads\n", argv[0]);
+        printf("       For single-threaded runs, pass threads = 1\n");
+        exit(1);
+    }
+
+    char *output_file = argv[2];
+    char *input_file = argv[1];
+    int threads = atoi(argv[3]);
+
+    g_thread_num = threads;
+
+    /* remove this in multithreaded version */
+    // if(threads != 1) {
+    //   printf("ERROR: Only supports single-threaded execution\n");
+    //   exit(1);
+    // }
+
+    struct img input;
+
+    if (!ppmb_read(input_file, &input.xsize, &input.ysize, &input.maxrgb,
+                   &input.r, &input.g, &input.b))
+    {
+        if (input.maxrgb > 255)
+        {
+            printf("Maxrgb %d not supported\n", input.maxrgb);
+            exit(1);
+        }
+
+        int *hist_r, *hist_g, *hist_b;
+
+        hist_r = (int *)calloc(input.maxrgb + 1, sizeof(int));
+        hist_g = (int *)calloc(input.maxrgb + 1, sizeof(int));
+        hist_b = (int *)calloc(input.maxrgb + 1, sizeof(int));
+
+        ggc::Timer t("histogram");
+
+        t.start();
+        histogram(&input, hist_r, hist_g, hist_b);
+        t.stop();
+
+        FILE *out = fopen(output_file, "w");
+        if (out)
+        {
+            print_histogram(out, hist_r, input.maxrgb);
+            print_histogram(out, hist_g, input.maxrgb);
+            print_histogram(out, hist_b, input.maxrgb);
+            fclose(out);
+        }
+        else
+        {
+            fprintf(stderr, "Unable to output!\n");
+        }
+        printf("Time: %llu ns\n", t.duration());
+    }
+}
